55f046a0a890287496e99793846830fc50fd7de6
Anooptripathe <anooptripathi0102@gmail.com>
2025-10-25 17:17:33 +0530

    Concept of Iterators

---
+++ iterator.py
@@ -0,0 +1,17 @@
+class abc:
+    def __init__(self):
+        self.num=1
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.num<=10:
+            val=self.num
+            self.num+=1
+            return val
+        else:
+            StopIteration
+
+value=abc()
+print(value.__next__())
